e8027dc8e6873fda36d454cd78a9d8c6c338998b
Apurba Biswas <apurba10024@gmail.com>
2018-11-14 20:03:12 +0600

    1.2 Added Radio Button response and changing image

--- app/src/main/java/com/example/apurba/turtles_cs193a_w2l1/MainActivity.java
+++ app/src/main/java/com/example/apurba/turtles_cs193a_w2l1/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.apurba.turtles_cs193a_w2l1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import stanford.androidlib.SimpleActivity;
 
@@ -11,5 +12,19 @@ public class MainActivity extends SimpleActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void updateImg(View view) {
+        int id = view.getId();
+        ImageView img = findViewById(R.id.turtule_img);
+        if (id == R.id.leo_button){
+            img.setImageResource(R.drawable.tmntleo);
+        }else if(id == R.id.mike_button){
+            img.setImageResource(R.drawable.tmntmike);
+        }else if(id == R.id.don_button){
+            img.setImageResource(R.drawable.tmntdon);
+        }else if(id == R.id.raph_button){
+            img.setImageResource(R.drawable.tmntraph);
+        }
     }
 }
