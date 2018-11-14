4be96edf4953dc637c1741e42b2fa217269fd01b
Apurba Biswas <apurba10024@gmail.com>
2018-11-14 19:10:20 +0600

    1.0 Added All files to the repository

---
+++ app/src/main/java/com/example/apurba/turtles_cs193a_w2l1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.apurba.turtles_cs193a_w2l1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
